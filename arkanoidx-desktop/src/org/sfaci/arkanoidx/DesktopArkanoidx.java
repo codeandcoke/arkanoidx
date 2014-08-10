fb908a9f05c59c1567dec4226dd8617e937e59ce
Santiago Faci <santiago.faci@gmail.com>
2014-08-10 16:03:22 +0200

    Now, ball die when comes to ground

---
+++ arkanoidx-desktop/src/org/sfaci/arkanoidx/DesktopArkanoidx.java
@@ -0,0 +1,24 @@
+package org.sfaci.arkanoidx;
+
+import org.sfaci.arkanoidx.util.Constants;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+/**
+ * Clase principal de la versi�n de escritorio (PC) del juego
+ * @author Santiago Faci
+ *
+ */
+public class DesktopArkanoidx {
+
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+		configuration.title = "Arkanoidx";
+
+		configuration.width = Constants.SCREEN_WIDTH;
+		configuration.height = Constants.SCREEN_HEIGHT;
+				
+		new LwjglApplication(new Arkanoidx(), configuration);
+	}
+}
